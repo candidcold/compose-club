669d5a80db67d47561a11530a5f5fb6963c6a775
David Morant <david.morant@nytimes.com>
2021-02-12 11:48:22 -0500

    2/12 Finish landing page button example

--- app/src/main/java/com/example/composeclub/PlpButtons.kt
+++ app/src/main/java/com/example/composeclub/PlpButtons.kt
@@ -7,6 +7,7 @@ import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -15,29 +16,59 @@ import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
-import com.example.composeclub.ui.theme.ComposeClubTheme
 import com.example.composeclub.ui.theme.pill
 import com.example.composeclub.ui.theme.plpBasic
 import java.util.*
 
+data class PricingModel(
+    val priceYouPay: String,
+    val wouldHavePaid: String? = null,
+    val savings: String? = null,
+)
+
+class PricingModelProvider : PreviewParameterProvider<PricingModel> {
+    override val values: Sequence<PricingModel>
+        get() = sequenceOf(
+            PricingModel(
+                priceYouPay = "$4.99/month",
+            ),
+            PricingModel(
+                priceYouPay = "$4.99/month",
+                savings = "Save 16%"
+            ),
+            PricingModel(
+                priceYouPay = "$4.99/month",
+                wouldHavePaid = "$16.99"
+            ),
+            PricingModel(
+                priceYouPay = "$4.99/month",
+                wouldHavePaid = "$16.99",
+                savings = "Save 16%"
+            ),
+        )
+}
+
+@Preview
 @Composable
 fun PlpButton(
-    priceYouPay: String,
-    wouldHavePaid: String? = null
+    @PreviewParameter(PricingModelProvider::class) pricingModel: PricingModel
 ) {
-    Column {
-        SavingsPill()
+    Box() {
         Button(
             colors = ButtonDefaults.buttonColors(
                 backgroundColor = plpBasic
             ),
-            modifier = Modifier.preferredWidth(width = 350.dp),
+            modifier = Modifier
+                .preferredWidth(width = 350.dp)
+                .padding(top = 10.dp),
             onClick = { }
         ) {
             Text(text = buildAnnotatedString {
                 append(
-                    wouldHavePaid,
+                    pricingModel.wouldHavePaid,
                     TextStyle(
                         color = Color.LightGray,
                         textDecoration = TextDecoration.LineThrough,
@@ -45,22 +76,33 @@ fun PlpButton(
                     )
                 )
                 append(
-                    priceYouPay,
+                    pricingModel.priceYouPay,
                     TextStyle(color = Color.White, fontWeight = FontWeight.Bold)
                 )
             })
         }
+        pricingModel.savings?.let {
+            SavingsPill(
+                it,
+                Modifier
+                    .align(alignment = Alignment.TopEnd)
+                    .padding(end = 8.dp)
+            )
+        }
     }
 }
 
-@Preview
 @Composable
-fun SavingsPill() {
+fun SavingsPill(
+    savings: String,
+    modifier: Modifier = Modifier
+) {
     Text(
-        text = "Save 16%".toUpperCase(Locale.getDefault()),
+        text = savings.toUpperCase(Locale.getDefault()),
         fontWeight = FontWeight.Bold,
         color = plpBasic,
-        modifier = Modifier
+        modifier = modifier
+            .preferredHeight(24.dp)
             .border(
                 width = 2.dp,
                 color = plpBasic,
@@ -68,25 +110,12 @@ fun SavingsPill() {
             )
             .clip(shape = pill)
             .background(color = Color.White)
-            .padding(horizontal = 10.dp, vertical = 5.dp)
+            .padding(horizontal = 8.dp, vertical = 2.dp)
     )
 }
 
 @Preview
 @Composable
-private fun PreviewButton() {
-    ComposeClubTheme {
-        PlpButton(priceYouPay = "$4.99/month")
-    }
-}
-
-@Preview
-@Composable
-private fun PreviewButton2() {
-    ComposeClubTheme {
-        PlpButton(
-            priceYouPay = "$4.99/month",
-            wouldHavePaid = "$16.99"
-        )
-    }
+private fun PreviewSavingsPill() {
+    SavingsPill(savings = "Save 16%")
 }
